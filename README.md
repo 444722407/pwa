5d98f861dfb8edf67758d8ca96fa8b475a63923f
chenyao <444722407@qq.com>
2025-01-06 14:18:43 +0800

    pwa 2种方式的安装

--- README.md
+++ README.md
@@ -1,9 +1,32 @@
-pwa 安装
-https://444722407.github.io/pwa/
+webmanifest: https://web.dev/learn/pwa/web-app-manifest?continue=https%3A%2F%2Fweb.dev%2Flearn%2Fpwa&hl=zh-cn#article-https://web.dev/learn/pwa/web-app-manifest&hl=zh-cn
+
+如果您未提供图标，或者图标的尺寸不符合建议的尺寸，
+
+则在某些平台上，您的应用将无法通过安装条件。
+
+在其他平台上，系统会自动生成图标，例如通过 PWA 的屏幕截图或使用通用图标。
 
 
-manifest 文件配置正确
+如果webmanifest.icons.purpose = maskable 您未提供可遮罩的图标
 
-手机谷歌浏览器要求最新
+则预期使用可遮罩图标的设备可能会缩小您的图标，并将其调整为所需的形状并采用白色背景。
 
-本地测试安装后，如果还想出现 要在浏览器设置-》投放、保存和分享-》在xxx（pwa）中打开 然后在pwa中打开，右上角卸载
+
+
+如果您的 PWA 可安装，并且您至少为 description 和 screenshots 字段提供了值，则安装对话框体验会从简单的“添加到主屏幕”信息栏转变为类似于应用商店中的更丰富的安装对话框。
+
+description 对 PWA 功能的说明。
+
+screenshots 包含 src、type 和 sizes（类似于 icons 对象）的屏幕截图对象数组，用于展示 PWA。没有大小限制。
+测试后：每张大小最好一样
+
+screenshots 规范：https://web.dev/patterns/web-apps/richer-install-ui?hl=zh-cn
+
+
+
+
+
+文档：https://web.dev/learn/pwa/installation-prompt?continue=https%3A%2F%2Fweb.dev%2Flearn%2Fpwa&hl=zh-cn#article-https://web.dev/learn/pwa/installation-prompt&hl=zh-cn
+
+
+demo:https://glitch.com/~mlearn-pwa-web-app-install-prompt
--- index.js
+++ index.js
@@ -1,40 +1,78 @@
-if ("serviceWorker" in navigator) {
-    // Register a service worker hosted at the root of the
-    // site using the default scope.
-    navigator.serviceWorker
-      .register("./sw.js")
-      .then((registration) => {
-        console.log("Service worker registration succeeded:", registration);
-        // At this point, you can optionally do something
-        // with registration. See https://developer.mozilla.org/zh-CN/docs/Web/API/ServiceWorkerRegistration
-      })
-      .catch((error) => {
-        console.error(`Service worker registration failed: ${error}`);
-      });
-  
-    // Independent of the registration, let's also display
-    // information about whether the current page is controlled
-    // by an existing service worker, and when that
-    // controller changes.
-  
-    // First, do a one-off check if there's currently a
-    // service worker in control.
-    if (navigator.serviceWorker.controller) {
-      console.log(
-        "This page is currently controlled by:",
-        navigator.serviceWorker.controller,
-      );
-    }
-  
-    // Then, register a handler to detect when a new or
-    // updated service worker takes control.
-    navigator.serviceWorker.oncontrollerchange = () => {
-      console.log(
-        "This page is now controlled by",
-        navigator.serviceWorker.controller,
-      );
-    };
+
+window.addEventListener("DOMContentLoaded", async event => {
+  if ('BeforeInstallPromptEvent' in window) {
+    console.log("支持BeforeInstallPromptEvent");
   } else {
-    console.log("Service workers are not supported.");
+    alert("不支持BeforeInstallPromptEvent");
   }
+
+  //安装条件
+  //Web 应用尚未安装
+  //--------可以在谷歌浏览器中右上角设置-----（投放，保存和分享）---在pwa中打开---右上角中卸载
+  //--------chrome://apps 也可卸载
+
+
+  //用户至少需要点击或点按页面一次（随时，即使是在前一次页面加载期间）
+  //用户需要在任何时间点查看该网页至少 30 秒
+  //通过 HTTPS 提供
+  //包含网络应用清单，其中包含：
+  //short_name 或 name
+  //icons - 必须包含 192 像素和 512 像素的图标
+  //start_url
+  //display - 必须是 fullscreen、standalone、minimal-ui 或 window-controls-overlay 中的一个
+  //prefer_related_applications 不得存在，也不得为 false
+  //由于其他原因（例如设备处于自助服务终端模式或没有权限），无法在当前设备上安装 PWA。
+
+  document.querySelector("#install").addEventListener("click", installApp);  //注册事件
+
+  // 弹窗方式
+  //某些浏览器（包括 description、categories 和 screenshots）会使用清单中的宣传字段来提升安装提示体验。
+  // 例如，使用 Android 版 Chrome 时，如果您的 PWA 为 description 和 screenshots 字段提供了值，
+  // 则安装对话框体验会从较小的添加到主屏幕信息栏转换为更大且更详细的对话框，类似于应用商店中的安装提示。
+
+
+ 
+});
+
+
+
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Chrome for Android 会向用户显示一个迷你信息栏，
+  // 但您可以通过对 beforeinstallprompt 事件调用 preventDefault() 来阻止此操作。
+  // 如果您未调用 preventDefault()，系统会在用户首次访问您的网站并在 Android 设备上满足可安装性条件时显示横幅，然后大约 90 天后再次显示。
+  e.preventDefault();
+  deferredPrompt = e;
+  // 最佳实践： 可以安装再把安装按钮显示出来
+  document.querySelector("#install").style.display="block";  
+  document.querySelector("#tips").style.display="none";  
   
+});
+
+//您可以使用 userChoice 属性来确定用户是否从您的界面中安装了您的应用。
+// 不过，如果用户通过地址栏或其他浏览器组件安装您的 PWA，userChoice 将不起作用。
+// 而是应监听 appinstalled 事件，该事件会在用户安装您的 PWA 时触发，无论使用哪种机制安装。
+
+window.addEventListener('appinstalled', (e) => {
+    console.log("appinstalled---安装成功");
+});
+
+async function installApp() {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    // Find out whether the user confirmed the installation or not
+    // const { outcome } = await deferredPrompt.userChoice;
+    // The deferredPrompt can only be used once.
+    deferredPrompt = null;  
+    // Act on the user's choice
+    // if (outcome === 'accepted') {
+    //     console.log("pwa 安装成功");
+    // } else if (outcome === 'dismissed') {
+    //     console.log("pwa 安装失败");
+    // }
+    // We hide the install button
+    document.querySelector("#install").style.display="none";
+
+  }
+}
+
